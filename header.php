a0dcfe8f7e00741b31b2c7319592328979876761
ajCelis <celis28shiny@gmail.com>
2020-10-12 19:07:41 -0500

    subiendo sistema

---
+++ header.php
@@ -0,0 +1,79 @@
+<?php // Example 26-2: header.php
+  session_start();
+
+  echo <<<_INIT
+  <!DOCTYPE html> 
+  <html>
+    <head>
+      <meta charset='utf-8'>
+      <meta name='viewport' content='width=device-width, initial-scale=1'>                 
+      <script src='node_modules/jquery/dist/jquery.min.js'></script>
+      <script src='node_modules/jquery-mobile/js/jquery.mobile.js'></script>
+      <script type="text/javascript" src="vendor/twbs/bootstrap/dist/js/bootstrap.min.js"></script>
+      <link href="vendor/twbs/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">        
+      <link rel='stylesheet' href='styles.css' type='text/css'>    
+      <script src='javascript.js'></script>
+  _INIT;
+  
+
+  require_once 'functions.php';
+
+  $userstr = 'Welcome Guest';
+
+  if (isset($_SESSION['user']))
+  {
+    $user     = $_SESSION['user'];
+    $loggedin = TRUE;
+    $userstr  = "Logged in as: $user";
+  }
+  else $loggedin = FALSE;
+
+echo <<<_MAIN
+    <title>Mi Red Social: $userstr</title>
+  </head>
+  <body>
+    <div data-role='page'>
+      <div data-role='header'>
+        <div id='logo' class='center'>Red social </div>
+        <div class='username'>$userstr</div>
+      </div>
+      <div data-role='content'>
+
+_MAIN;
+
+  if ($loggedin)
+  {
+echo <<<_LOGGEDIN
+        <div class='center'>
+          <a data-role='button' data-inline='true' data-icon='home'
+            data-transition="slide" href='members.php?view=$user'>Home</a>
+          <a data-role='button' data-inline='true' data-icon='user'
+            data-transition="slide" href='members.php'>Members</a>
+          <a data-role='button' data-inline='true' data-icon='heart'
+            data-transition="slide" href='friends.php'>Friends</a><br>
+          <a data-role='button' data-inline='true' data-icon='mail'
+            data-transition="slide" href='messages.php'>Messages</a>
+          <a data-role='button' data-inline='true' data-icon='edit'
+            data-transition="slide" href='profile.php'>Edit Profile</a>
+          <a data-role='button' data-inline='true' data-icon='action'
+            data-transition="slide" href='logout.php'>Log out</a>
+        </div>
+        
+_LOGGEDIN;
+  }
+  else
+  {
+echo <<<_GUEST
+        <div class='center'>
+          <a data-role='button' data-inline='true' data-icon='home'
+            data-transition='slide' href='index.php'>Home</a>
+          <a data-role='button' data-inline='true' data-icon='plus'
+            data-transition="slide" href='signup.php'>Sign Up</a>
+          <a data-role='button' data-inline='true' data-icon='check'
+            data-transition="slide" href='login.php'>Log In</a>
+        </div>
+        <p class='info'>(You must be logged in to use this app)</p>
+        
+_GUEST;
+  }
+?>
